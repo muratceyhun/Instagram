236e439866f8a66144ca0b3df805aacc6e49ff0a
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-02-18 21:01:41 +0300

    updates

--- InstagramClone/SignUpController.swift
+++ InstagramClone/SignUpController.swift
@@ -14,10 +14,8 @@ class SignUpController: UIViewController {
   
     
     let btnAddPhoto : UIButton = {
-       
         let btn = UIButton(type: .system)
         btn.setImage(#imageLiteral(resourceName: "Choose_Photo").withRenderingMode(.alwaysOriginal), for: .normal)
-        
         btn.addTarget(self, action: #selector(btnAddPhotoPressed), for: .touchUpInside)
         return btn
         
