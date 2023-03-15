01ef8b2c7fadcb6ce3fd791efcd073d6a1fa4722
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-03-15 23:34:38 +0300

    updates

--- InstagramClone/UserProfile/UserProfileController.swift
+++ InstagramClone/UserProfile/UserProfileController.swift
@@ -17,7 +17,6 @@ class UserProfileController : UICollectionViewController {
     var userID : String?
     let postCellID = "postCellID"
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
